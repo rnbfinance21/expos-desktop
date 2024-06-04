a376d868dc22874ab11972e4aaf6f5c0773d03ed
luthfi1290 <mochluthfi028@gmail.com>
2024-06-04 20:26:49 +0700

    fix ucwords

--- renderer/utils/string.ts
+++ renderer/utils/string.ts
@@ -8,7 +8,7 @@ export const classNames = (...classes: unknown[]) => {
 };
 
 export const ucwords = (str: string) => {
-  str = str.toLowerCase().replace(/\b[a-z]/g, function (letter) {
+  str = str?.toLowerCase().replace(/\b[a-z]/g, function (letter) {
     return letter.toUpperCase();
   });
   return str;
